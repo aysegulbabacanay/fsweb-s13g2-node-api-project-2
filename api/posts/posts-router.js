a87924d8b9594d573504756bf9b1242a0536885a
Aysegul <aysglbabacan@gmail.com>
2023-01-26 00:04:07 +0300

    ödev

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,129 @@
 // post routerları buraya yazın
+
+const express = require("express");
+
+const Post = require("./posts-model")  // burası database url i buradan al diyoruz 
+
+const server= express();
+const router =express.Router();
+
+
+router.get("/", (req,res)=>{
+Post.find()
+.then(found=>{
+res.json(found)
+})
+.catch(err=>{
+res.status(500).json({
+    message:"Gönderiler alınamadı"
+})
+})
+
+})
+
+router.get("/:id", (req,res)=>{
+
+Post.findById(req.params.id)  // bu fonk bizden id bekliyor bunu da req.params.id den alacağız.
+.then(found=>{
+    if(!found){
+        res.status(404).json({
+            message:"Belirtilen ID'li gönderi bulunamadı"
+        })
+    }
+    else{
+        res.status(200).json(found)
+    }
+})
+.catch(err=>{
+    res.status(500).json({
+        message:"Gönderi bilgisi alınamadı"
+})
+})
+})
+
+router.post("/", (req,res)=>{
+    const{title,contents}=req.body;
+
+    if(!title || !contents){  //biz postman de title ve content objesi oluşturacağız sorgu atarken,bdoy-json
+res.status(400).json({
+    message: "Lütfen gönderi için bir title ve contents sağlayın"
+})
+    }
+    else{
+        Post.insert({title,contents})  //buranın sonucunda id dönüyor. sonrasında bu id li postu bul diyoruz.
+         .then(({id})=>{
+             return Post.findById(id)
+            
+         })
+        .then(post=>{
+            res.status(201).json(post)  // kullanıcıyı oluşturduğumuz için bunu direkt kullandık ondan boş.
+        })
+    
+        .catch(err=>{
+            res.status(500).json({
+                message:"Veritabanına kaydedilirken bir hata oluştu"
+        })
+       
+    })
+}  // buraya kadar hepsi else in içinde
+})
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const data = await Post.findById(req.params.id)
+
+        if (!data) {
+            res.status(404).json({
+                message: "Belirtilen ID li gönderi bulunamadı"
+            })
+        }
+        else {
+            await Post.remove(req.params.id);  //tekrar await dememizin nedeni bunu yazmassak delete işleminden sonrs get dersek hala görünüyor
+            res.status(200).json(data)
+
+            // const a = await Post.remove(req.params.id);  
+            // res.status(200).json(a) böyle olunca neden 1 olarak döndü?
+        }
+    }
+
+    catch (err) {  //  try içinde arrow func. gibi yazmıyoruz.
+        res.status(500).json({
+            message: "Gönderi silinemedi"
+        })
+    }
+
+})
+
+router.put("/:id", async(req,res)=>{
+try{
+
+    const data = await Post.findById(req.params.id)
+
+    if(!data){
+        res.status(404).json({
+            message: "Belirtilen ID li gönderi bulunamadı"
+        })
+    }
+    else{
+        const{title,contents}=req.body;
+
+        if(!title || !contents){  
+    res.status(400).json({
+        message: "Lütfen gönderi için bir title ve contents sağlayın"
+    })
+        }
+        else{
+            await Post.update()
+        }
+    } // else in içi burda bitiyor
+    
+}
+    catch (err) {  //  try içinde arrow func. gibi yazmıyoruz.
+        res.status(500).json({
+            message: "Gönderi bilgileri güncellenemedi"
+        })
+    }
+
+})
+
+module.exports = router;
